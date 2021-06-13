7b174a7a235c4c9a26c2faa0ea64d13f8cdf17af
local_notebook <21600220@handong.edu>
2021-06-13 10:33:27 +0200

    21.06.13

---
+++ (9) 재귀/[02447] 별 찍기 - 10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void star(int num) {
+    if(num == 1) printf("*");
+    else for(int i = 0; i < num; i++) {
+        
+    }
+}
+
+int main() {
+    
+}
