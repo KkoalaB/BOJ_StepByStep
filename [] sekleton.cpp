ca4b1550ffeaec548d01ff91ded10691f5582e4e
local_notebook <21600220@handong.edu>
2021-06-22 23:43:10 +0200

    21.06.22

--- [] sekleton.cpp
+++ [] sekleton.cpp
@@ -26,6 +26,47 @@ void insertionSort(int arr[], int size) {
     }
 }
 
+// Merge sort
+void merge(int* arr, int* sortedArr, int begin, int middle, int end) {
+    int sortedInd = begin;
+    int i = begin;
+    int j = middle+1;
+
+
+    while (i <= middle && j <= end) {
+        if (arr[i] < arr[j]) sortedArr[sortedInd] = arr[i++];
+        else  sortedArr[sortedInd] = arr[j++];
+
+        sortedInd++;
+    }
+
+    //왼쪽이 오른쪽보다 모두 작은 경우
+    if (i > middle) {
+        while (j <= end) {
+            sortedArr[sortedInd++] = arr[j++];
+        }
+    }
+    //오른쪽이 왼쪽보다 모두 작은 경우
+    if (j > end) {
+        while (i <= middle) {
+            sortedArr[sortedInd++] = arr[i++];
+        }
+    }
+
+    for (int k = begin; k <= end; k++) arr[k] = sortedArr[k];
+
+}
+
+void mergeSort(int* arr, int* sortedArr, int begin, int end) {
+    int middle;
+    if (begin<end) {
+        middle = (begin + end) / 2;
+        mergeSort(arr, sortedArr, begin, middle);
+        mergeSort(arr, sortedArr, middle + 1, end);
+        merge(arr, sortedArr, begin, middle, end);
+    }
+}
+
 */
 
 /* math.h
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector> // https://blockdmask.tistory.com/70
+#include <list>
+#include <map>
+#include <set>
+using namespace std;
+
+int main() {
+    printf("%f\n", (float)7/5);
+    printf("%.0f\n", (float)7/5);
+}
