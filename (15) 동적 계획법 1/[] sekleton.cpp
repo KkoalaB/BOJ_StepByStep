5ea8797890ddc5a60fafafae4319fcf26a78ede2
local_notebook <21600220@handong.edu>
2021-08-02 11:25:15 +0200

    21.08.01

---
+++ (15) 동적 계획법 1/[] sekleton.cpp
@@ -0,0 +1,43 @@
+/*
+ * Author: Byeongmin Ryoo
+ * Blog: https://velog.io/@kkoala
+ * Contact: bmformail@naver.com
+ */
+
+#include <iostream>
+#include <utility> // pair
+#include <algorithm>
+
+/* STL
+#include <vector> // https://blockdmask.tistory.com/70
+#include <list>
+#include <map>
+#include <set>
+*/
+
+/* math.h
+#define _USE_MATH_DEFINES // need this to use math defines
+#include <math.h>
+*/
+
+using namespace std;
+
+/* 조건 */
+
+/* 변수 */
+
+/* 함수 */
+
+int main() {
+    /* Fast cin cout */
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    /*****************/
+
+    /* 입력 */
+
+    /* 풀이 */
+
+    /* 출력 */
+}
