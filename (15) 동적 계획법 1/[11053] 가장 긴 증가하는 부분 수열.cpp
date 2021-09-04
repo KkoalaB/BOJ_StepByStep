5a6734294c0551a04e35c7167f918a2075432649
local_notebook <21600220@handong.edu>
2021-09-04 18:38:13 +0200

    21.09.04

--- (15) 동적 계획법 1/[11053] 가장 긴 증가하는 부분 수열.cpp
+++ (15) 동적 계획법 1/[11053] 가장 긴 증가하는 부분 수열.cpp
@@ -1,31 +1,49 @@
-#include <cstdio>
-#include <algorithm>
+/*
+ * Author: Byeongmin Ryoo
+ * Blog: https://velog.io/@kkoala
+ * Contact: bmformail@naver.com
+ */
+
+#include <iostream>
+#include <cstring> // memset
 
 using namespace std;
 
+/* 조건 */
+#define MAX_N 10001
+
+/* 변수 */
+int N;
+int seq[MAX_N], memo[MAX_N];
+
+/* 함수 */
+int max3(int a, int b, int c) {
+    return max(a, max(b, c));
+}
+
+int dp(int idx) {
+    if(idx < 0) return 0;
+    int& ret = memo[idx];
+    if(ret > -1) return ret;
+    return ret = max3(dp(idx-1), seq[idx] + dp(idx-2), seq[idx] + seq[idx-1] + dp(idx-3));
+}
+
 int main() {
-    int n, k, arr[1000], dp[1000];
-    int max = 1;
-    scanf("%d", &n);
+    /* Fast cin cout */
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    /*****************/
 
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &k);
-        arr[i] = k;
+    /* 입력 */
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        cin >> seq[i];
     }
 
-    for (int i = 0; i < n; i++) {
-        dp[i] = 1;
-        for(int j = 0; j < i; j++) {
-            if(arr[j] < arr[i] && dp[j]+1 > dp[i]) {
-                dp[i] = dp[j] + 1;
-                if(max < dp[i]) {
-                    max = dp[i];
-                }
-            }
-        }
-    }
+    /* 풀이 */
+    memset(memo, -1, sizeof(memo));
 
-    printf("%d", max);
-
-    return 0;
+    /* 출력 */
+    cout << dp(N-1) << '\n';
 }
