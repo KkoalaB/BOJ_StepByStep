495ec45112ec0ab2c76f764e215cb8b9b8fe9ff5
local_notebook <21600220@handong.edu>
2021-10-24 23:16:37 +0200

    21.10..24

---
+++ (15) 동적 계획법 1/[12865] 평범한 배낭.cpp
@@ -0,0 +1,91 @@
+/*
+ * Author: Byeongmin Ryoo
+ * Blog: https://velog.io/@kkoala
+ * Contact: bmformail@naver.com
+ */
+
+#include <iostream>
+#include <utility> // pair
+#include <algorithm>
+
+/* STL
+#include <vector> // https://blockdmask.tistory.com/70
+#include <list>
+#include <map>
+#include <set>
+*/
+
+/* math.h
+#define _USE_MATH_DEFINES // need this to use math defines
+#include <math.h>
+*/
+
+//#include <string>
+/* 배열 초기화 memset
+    //byte-by-byte라서 int는 -1이나 0이 아니면 안됨
+    #include <cstring>
+    memset(arr, 0, sizeof(arr));
+*/
+
+#include <climits>
+/* <climits>
+CHAR_BIT    char의 비트 수
+SCHAR_MIN   signed char의 최소값
+SCHAR_MAX   signed char의 최대값
+UCHAR_MAX   unsigned char의 최대값
+CHAR_MIN    char의 최소값
+CHAR_MAX    char의 최대값
+MB_LEN_MAX  멀티바이트 문자의 최대 바이트 수
+SHRT_MIN    short int의 최소값
+SHRT_MAX    short int의 최대값
+USHRT_MAX   unsigned short int의 최대값
+INT_MIN     int의 최소값
+INT_MAX     int의 최대값
+UINT_MAX    unsigned int의 최대값
+LONG_MIN    long int의 최소값
+LONG_MAX    long int의 최대값
+ULONG_MAX   unsigned long int의 최대값
+*/
+
+using namespace std;
+
+/* 조건 */
+#define MAX_N 101
+#define MAX_K 100001
+#define MAX_W 100001
+#define MAX_V 1001
+
+/* 변수 */
+int N, K;
+int W[MAX_N], V[MAX_N];
+int result = 0;
+
+/* 함수 */
+int dp(int n, int k) {
+    if(n == 0 || k <= 0) {
+        if(k >= W[n]) return V[n];
+        return 0;
+    }
+    
+    if(K < W[n]) return dp(n-1, k);
+    return max(dp(n-1, k-W[n]) + V[n], dp(n-1, k));
+}
+
+int main() {
+    /* Fast cin cout */
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    /*****************/
+
+    /* 입력 */
+    cin >> N >> K;
+    for(int i = 0; i < N; i++) {
+        cin >> W[i] >> V[i];
+    }
+
+    /* 풀이 */
+
+    /* 출력 */
+    cout << dp(N-1, K) << '\n';
+}
