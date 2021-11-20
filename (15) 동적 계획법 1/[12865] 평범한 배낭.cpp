078a5fc3d763255801c07e83732a713fedc61e8e
local_notebook <21600220@handong.edu>
2021-11-20 15:34:07 +0100

    21.11.20

--- (15) 동적 계획법 1/[12865] 평범한 배낭.cpp
+++ (15) 동적 계획법 1/[12865] 평범한 배낭.cpp
@@ -5,47 +5,6 @@
  */
 
 #include <iostream>
-#include <utility> // pair
-#include <algorithm>
-
-/* STL
-#include <vector> // https://blockdmask.tistory.com/70
-#include <list>
-#include <map>
-#include <set>
-*/
-
-/* math.h
-#define _USE_MATH_DEFINES // need this to use math defines
-#include <math.h>
-*/
-
-//#include <string>
-/* 배열 초기화 memset
-    //byte-by-byte라서 int는 -1이나 0이 아니면 안됨
-    #include <cstring>
-    memset(arr, 0, sizeof(arr));
-*/
-
-#include <climits>
-/* <climits>
-CHAR_BIT    char의 비트 수
-SCHAR_MIN   signed char의 최소값
-SCHAR_MAX   signed char의 최대값
-UCHAR_MAX   unsigned char의 최대값
-CHAR_MIN    char의 최소값
-CHAR_MAX    char의 최대값
-MB_LEN_MAX  멀티바이트 문자의 최대 바이트 수
-SHRT_MIN    short int의 최소값
-SHRT_MAX    short int의 최대값
-USHRT_MAX   unsigned short int의 최대값
-INT_MIN     int의 최소값
-INT_MAX     int의 최대값
-UINT_MAX    unsigned int의 최대값
-LONG_MIN    long int의 최소값
-LONG_MAX    long int의 최대값
-ULONG_MAX   unsigned long int의 최대값
-*/
 
 using namespace std;
 
