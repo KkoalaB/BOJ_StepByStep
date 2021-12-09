d288f1cf42ea030782b38ba980e11c46945100eb
local_notebook <21600220@handong.edu>
2021-12-09 22:50:11 +0100

    21.12.09

--- sources/functions.cpp
+++ sources/functions.cpp
@@ -1,11 +1,13 @@
 /* 최대공약수 최소공배수 */
 // 최대공약수 Greatest Common Divisor
+// 유클리드 호제법(euclidean algorithm) 사용.
 int GCD(int a, int b) {
     if(a % b == 0) return b;
     return GCD(b, a%b);
 }
 
 // 최소공배수 Least Common Multiple
+// GCD 함수 필요
 int LCM(int a, int b) {
     return a * b / GCD(a, b);
 }
