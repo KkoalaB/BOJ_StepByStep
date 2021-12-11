800f4bb9e7951ac6df4fbfebd910dbbf2616a752
local_notebook <21600220@handong.edu>
2021-12-11 18:40:55 +0100

    21.12.11

--- sources/functions.cpp
+++ sources/functions.cpp
@@ -11,3 +11,15 @@ int GCD(int a, int b) {
 int LCM(int a, int b) {
     return a * b / GCD(a, b);
 }
+
+// 순열 조합
+// nCk 같은 조합 구하는 함수
+int combination(int n, int k) {
+    int C[0][0]; // 사용시 이 부분 지우고 전역으로 C[][] 배열 선언 필요
+    if(C[n][k] > 0) return C[n][k];
+    if(C[n][n-k] > 0) return C[n][k] = C[n][n-k];
+    if(n == 0) return C[n][k] = 0;
+    if(n == k) return C[n][k] = 1;
+    if(k == 0) return C[n][k] = 1;
+    return C[n][k] = combination(n-1, k-1) + combination(n-1, k);
+}
