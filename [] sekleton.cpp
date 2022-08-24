ac71fe26f11dc5218b444c8e260d5a65ed5c1063
Byeongmin <bmryu0501>
2022-08-24 14:55:27 +0900

    'skeleton

--- [] sekleton.cpp
+++ [] sekleton.cpp
@@ -1,11 +1,4 @@
-/*
- * Author: Byeongmin Ryoo
- * Blog: https://velog.io/@kkoala
- * Contact: bmformail@naver.com
- */
-
 #include <iostream>
-//#include <utility> // pair
 //#include <algorithm>
 
 /* STL
@@ -22,51 +15,55 @@
 #include <math.h>
 */
 
-//#include <string>
-/* 배열 초기화 memset
-    // byte-by-byte라서 int는 -1이나 0이 아니면 안됨
+//#include <cstring>
+/* memset
+    // Since byte-by-byte, int should be 0 or -1
     #include <cstring>
     memset(arr, 0, sizeof(arr));
 */
 
 //#include <climits>
 /* <climits>
-CHAR_BIT    char의 비트 수
-SCHAR_MIN   signed char의 최소값
-SCHAR_MAX   signed char의 최대값
-UCHAR_MAX   unsigned char의 최대값
-CHAR_MIN    char의 최소값
-CHAR_MAX    char의 최대값
-MB_LEN_MAX  멀티바이트 문자의 최대 바이트 수
-SHRT_MIN    short int의 최소값
-SHRT_MAX    short int의 최대값
-USHRT_MAX   unsigned short int의 최대값
-INT_MIN     int의 최소값
-INT_MAX     int의 최대값
-UINT_MAX    unsigned int의 최대값
-LONG_MIN    long int의 최소값
-LONG_MAX    long int의 최대값
-ULONG_MAX   unsigned long int의 최대값
+CHAR_BIT    char number of bit 
+SCHAR_MIN   signed char
+SCHAR_MAX   signed char
+UCHAR_MAX   unsigned char
+CHAR_MIN    char
+CHAR_MAX    char
+MB_LEN_MAX  multi-byte char max bite
+SHRT_MIN    short int
+SHRT_MAX    short int
+USHRT_MAX   unsigned short int
+INT_MIN     int
+INT_MAX     int
+UINT_MAX    unsigned int
+LONG_MIN    long int
+LONG_MAX    long int
+ULONG_MAX   unsigned long int
+*/
+
+/* customized */ /*
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+int dx8[8] = {1, 1, 1, -1, -1, -1, 0, 0};
+int dy8[8] = {-1, 0, 1, -1, 0, 1, -1, 1};
 */
 
 using namespace std;
 
-/* 조건 */
+/* condition */
 
-/* 변수 */
+/* variables */
 
-/* 함수 */
+/* functions */
 
 int main() {
-    /* Fast cin cout */
-    ios_base :: sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    /*****************/
+    ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    /* init */
 
-    /* 입력 */
+    /* input */
 
-    /* 풀이 */
+    /* solving */
 
-    /* 출력 */
+    /* output */
 }
